653e84123d0aedd4066213f4c44e40a318011a16
prashant@brainmobi.com <prashant@brainmobi.com>
2018-06-19 19:58:10 +0530

    firdt git comit for my code

---
+++ src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -0,0 +1,52 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import com.crm.qa.base.TestBase;
+
+public class ContactsPage extends TestBase {
+	
+	@FindBy(xpath="//td[contains(text(),'Contacts')]")
+	WebElement contactslabel;
+	
+	@FindBy(id="first_name")
+	WebElement firstName;
+	
+	@FindBy(id="surname")
+	WebElement lastName;
+	
+	@FindBy(name="client_lookup")
+	WebElement companyName;
+	
+	@FindBy(xpath="//input[@type='submit'and @value='Save']")
+	WebElement savebtn;
+	
+	public ContactsPage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	public boolean verifyContactsLabel(){
+		return contactslabel.isDisplayed();
+	}
+	
+	public void selectContactsByNumber(String num){
+		driver.findElement(By.xpath("//table[@class='datacard']/tbody/tr[5]/td["+num+"]")).click();
+		
+	}
+	
+	public void createNewContact(String title, String ftName, String ltName, String compName){
+		
+		Select select = new Select(driver.findElement(By.name("title")));
+		select.selectByVisibleText(title);
+		
+		firstName.sendKeys(ftName);
+		lastName.sendKeys(ltName);
+		companyName.sendKeys(compName);
+		savebtn.click();
+		
+	}
+}
---
+++ src/test/java/com/crm/qa/testcases/ContactsPageTest.java
@@ -0,0 +1,67 @@
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.ContactsPage;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
+
+public class ContactsPageTest extends TestBase{
+	
+	LoginPage loginPage;
+	HomePage homePage;
+	TestUtil testUtil;
+	ContactsPage contactsPage;
+	
+	String sheetName = "contacts";
+	
+	public ContactsPageTest(){
+		super();
+	}
+	
+	@BeforeMethod
+	public void setup(){
+		
+		initialization();
+		testUtil = new TestUtil();
+		contactsPage = new ContactsPage();
+		loginPage = new LoginPage();
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		testUtil.switchToFrame();
+		contactsPage = homePage.clickOnContactsLink();
+	}
+
+	@Test(priority=1)
+	public void verifyContactsPageLabelTest(){
+		Assert.assertTrue(contactsPage.verifyContactsLabel(),"Contact label missing");
+	}
+	
+	@Test(priority=2)
+	public void selectContestTest(){
+		contactsPage.selectContactsByNumber("1");
+	}
+	
+	@DataProvider
+	public Object[][] getCRMTestData(){
+		Object data[][] = TestUtil.getTestData(sheetName);
+		return data;
+	}
+	
+	@Test(priority=3, dataProvider="getCRMTestData")
+	public void validateCreateNewContactTest(String title, String ftName, String ltName, String compName){
+		homePage.clickOnNewContactLink();
+		contactsPage.createNewContact(title, ftName, ltName, compName);
+	}
+	
+	@AfterMethod
+	public void teardown(){
+		driver.quit();
+	}
+
+}
